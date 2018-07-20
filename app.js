d20d74b7a6274326f2557d9df6ba6ec1f9813f2f
zecampos <joseguilhermesantoscampos@gmail.com>
2018-07-20 16:23:41 -0300

    implementando pdf

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const nodemailer = require('nodemailer');
 const cors = require('cors')
+const fs = require('fs-extra');
+const Pdf = require('./pdf.js')
+const fss = require('fs');
 const app = express()
-
-
-app.use(bodyParser.json({limit: '50mb'}))
-app.use(bodyParser.urlencoded({limit: '50mb', extended: false }))
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }))
 app.use(cors())
-app.use((req, res, next) =>{
+app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', ['*']);
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
@@ -18,11 +19,11 @@ app.get('/', (req, res) => {
     res.send('Hello')
 })
 
+
 app.post('/send', (req, res) => {
-    const output = `
-    <img src="${req.body.src}" />
-    `;
+    Pdf.generate(req.body.src)
     nodemailer.createTestAccount((err, account) => {
+
         // create reusable transporter object using the default SMTP transport
         let transporter = nodemailer.createTransport({
             host: 'mail.ensaiogospel.com',
@@ -32,8 +33,8 @@ app.post('/send', (req, res) => {
                 user: 'financeiro@ensaiogospel.com', // generated ethereal user
                 pass: 'Jose_2505' // generated ethereal password
             },
-            tls:{
-                rejectUnauthorized:false
+            tls: {
+                rejectUnauthorized: false
             }
         });
 
@@ -43,23 +44,35 @@ app.post('/send', (req, res) => {
             to: 'joseguilhermesantoscampos@gmail.com', // list of receivers
             subject: 'Nova Impressão', // Subject line
             text: '', // plain text body
-            html: output // html body
+            html: '',
+            attachments: [
+                {   // file on disk as an attachment
+                    filename: 'print.pdf',
+                    path: './src/files/print.pdf' // stream this file
+                }
+            ]
         };
-
         // send mail with defined transport object
         transporter.sendMail(mailOptions, (error, info) => {
+
             if (error) {
                 return console.log(error);
-                res.send({msg: 'Erro ao enviar'})
+                res.send({ msg: 'Erro ao enviar' })
             }
+           
             console.log('Message sent: %s', info.messageId);
             console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-            res.send({msg: 'Enviado com Sucesso!!!', body: req.body.src})
-
+            res.send({ msg: 'Enviado com Sucesso!!!' })
+            fs.unlink('src/files/print.pdf', (err) =>{
+                if(err){console.log(err)}
+                console.log('deletado')
+            })
 
         });
+
     });
+     
 
 })
 var port = process.env.PORT || 3000
-app.listen(port , function() { console.log(' server started')})
+app.listen(port, function () { console.log(' server started') })
---
+++ pdf.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer');
+const moment = require('moment')
+const fs = require('fs');
+const image = `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAAAAACPAi4CAAAAB3RJTUUH1QEHDxEhOnxCRgAAAAlwSFlzAAAK8AAACvABQqw0mAAAAXBJREFUeNrtV0FywzAIxJ3+K/pZyctKXqamji0htEik9qEHc3JkWC2LRPCS6Zh9HIy/AP4FwKf75iHEr6eU6Mt1WzIOFjFL7IFkYBx3zWBVkkeXAUCXwl1tvz2qdBLfJrzK7ixNUmVdTIAB8PMtxHgAsFNNkoExRKA+HocriOQAiC+1kShhACwSRGAEwPP96zYIoE8Pmph9qEWWKcCWRAfA/mkfJ0F6dSoA8KW3CRhn3ZHcW2is9VOsAgoqHblncAsyaCgcbqpUZQnWoGTcp/AnuwCoOUjhIvCvN59UBeoPZ/AYyLm3cWVAjxhpqREVaP0974iVwH51d4AVNaSC8TRNNYDQEFdlzDW9ob10YlvGQm0mQ+elSpcCCBtDgQD7cDFojdx7NIeHJkqi96cOGNkfZOroZsHtlPYoR7TOp3Vmfa5+49uoSSRyjfvc0A1kLx4KC6sNSeDieD1AWhrJLe0y+uy7b9GjP83l+m68AJ72AwSRPN5g7uwUAAAAAElFTkSuQmCC`
+
+
+
+
+async function generate(data) {
+
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage()
+        const arquivo = `<img src="${data}" />`
+        const fileData = moment().format('HH-mm DD-MM-YYYY')
+        await page.setContent(arquivo);
+        await page.emulateMedia('print')
+        await page.pdf({
+            path: `./src/files/print.pdf`,
+            format: 'A4',
+        })
+        console.log('done')
+      
+        await browser.close()
+
+
+    }
+    catch (e) {
+        console.log(' our error', e)
+    }
+   
+}
+
+module.exports = { generate}
